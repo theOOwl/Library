8df8f8bc445730639e4bd1f9b358cedd88283b79
Pouria <100228221+theOOwl@users.noreply.github.com>
2023-07-29 17:00:49 +0330

    First Commit For ibrary Project

---
+++ library/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace library
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Writer { get; set; }
+        public string Subject { get; set; }
+    }
+}
---
+++ library/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace library
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Library library  = new Library();
+           
+        }
+    }
+}
