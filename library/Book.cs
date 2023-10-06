4b68feb03ba5db8849d04a0ef7333e043a568699
Pouria <100228221+theOOwl@users.noreply.github.com>
2023-10-07 01:59:52 +0330

    init the test project

--- library/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace library
-{
-    public class Book
-    {
-        public string Name { get; set; }
-        public string Writer { get; set; }
-        public string Subject { get; set; }
-    }
-}
